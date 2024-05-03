0a5b35558d8bc2021dae55fe263ea4f8717fc95e
Sadiasabit <sadiamahmud71@gmail.com>
2024-05-03 12:02:28 +0600

    function

---
+++ index.js
@@ -0,0 +1,41 @@
+// Rule__1 //
+
+function myFunction(){
+    console.log("My name is Ema");
+};
+myFunction();
+
+// Rule__2 //
+
+function myFunction2(name){
+    console.log("My name is "+name);
+}
+myFunction2("Sabit");
+myFunction2("Albid");
+myFunction2("Noman");
+
+// Rule__3 //
+
+function myFunction3(name,age){
+    console.log("My name is"+name+"I am "+age+" years");
+};
+myFunction3("Ratul",21);
+myFunction3("Nayeem",22);
+
+// Rule__4 return//
+
+function myFunction4(){
+    console.log("My name.");
+    return 5;
+    console.log(5);
+};
+myFunction4();
+
+// Rule__5 Error//
+
+function myFunction5(){
+    let x=5;
+    console.log(x);
+};
+console.log(x);
+
